0fb3e4cda3e399f7ce24081c7fbddc339c80ef1b
LemADEC <lemadec.fr@gmail.com>
2015-11-13 21:23:09 +0100

    Code cleanup
    Minor optimisation in teleporter

--- src/main/java/cr0s/warpdrive/world/SpaceTeleporter.java
+++ src/main/java/cr0s/warpdrive/world/SpaceTeleporter.java
@@ -5,42 +5,39 @@ import net.minecraft.world.Teleporter;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
-public class SpaceTeleporter extends Teleporter
-{
-    int x, y, z;
-    int orientation;
-    World world;
-
-    public SpaceTeleporter(WorldServer par1WorldServer, int orientation, int x, int y, int z)
-    {
-        super(par1WorldServer);
-        this.orientation = orientation;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        world = par1WorldServer;
-    }
-
-    /**
-     * Create a new portal near an entity.
-     */
-    @Override
-    public void placeInPortal(Entity par1Entity, double par2, double par4, double par6, float par8)
-    {
-        //EntityPlayer player = (EntityPlayer) par1Entity;
-        //player.setWorld(world);
-        //player.setPositionAndUpdate(x, y, z);
-    }
-
-    @Override
-    public boolean makePortal(Entity par1Entity)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean placeInExistingPortal(Entity par1Entity, double par2, double par4, double par6, float par8)
-    {
-        return false;
-    }
+public class SpaceTeleporter extends Teleporter {
+	int x, y, z;
+	int orientation;
+	World world;
+	
+	public SpaceTeleporter(WorldServer worldServer, int orientation, int x, int y, int z) {
+		super(worldServer);
+		this.orientation = orientation;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		world = worldServer;
+	}
+	
+	@Override
+	public void placeInPortal(Entity par1Entity, double par2, double par4, double par6, float par8) {
+		//EntityPlayer player = (EntityPlayer) par1Entity;
+		//player.setWorld(world);
+		//player.setPositionAndUpdate(x, y, z);
+	}
+	
+	@Override
+	public boolean placeInExistingPortal(Entity par1Entity, double par2, double par4, double par6, float par8) {
+		return true;
+	}
+	
+	@Override
+	public boolean makePortal(Entity par1Entity) {
+		return true;
+	}
+	
+	@Override
+	public void removeStalePortalLocations(long p_85189_1_) {
+		// do nothing
+	}
 }
