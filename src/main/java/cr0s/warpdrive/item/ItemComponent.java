e09a6d11746f1ecf7a08edccf29061a15e933bf6
LemADEC <lemadec.fr@gmail.com>
2015-12-06 20:05:16 +0100

    Added new components (code)
    Existing ones have been reallocated

--- src/main/java/cr0s/warpdrive/item/ItemComponent.java
+++ src/main/java/cr0s/warpdrive/item/ItemComponent.java
@@ -4,169 +4,97 @@ import java.util.List;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
-import net.minecraftforge.oredict.ShapedOreRecipe;
-import cpw.mods.fml.common.registry.GameRegistry;
 import cr0s.warpdrive.WarpDrive;
 import cr0s.warpdrive.api.IAirCanister;
+import cr0s.warpdrive.data.ComponentType;
 
 public class ItemComponent extends Item implements IAirCanister {	
-	private IIcon[] potentialIcons;
-	private String[] potentialUnlocalized = new String[9];
-	private ItemStack[] cachedIS;
+	private IIcon[] icons;
+	private static ItemStack[] itemStackCache;
 	
 	public ItemComponent() {
 		super();
 		setHasSubtypes(true);
-		setUnlocalizedName("warpdrive.crafting.Malformed");
+		setUnlocalizedName("warpdrive.crafting.component");
 		setCreativeTab(WarpDrive.creativeTabWarpDrive);
 		
-		potentialUnlocalized[0] = "EmptyCore";
-		potentialUnlocalized[1] = "TeleCore";
-		potentialUnlocalized[2] = "WarpCore";
-		potentialUnlocalized[3] = "LaserCore";
-		potentialUnlocalized[4] = "ReactorCore";
-		potentialUnlocalized[5] = "InterfaceComputer";
-		potentialUnlocalized[6] = "InterfacePower";
-		potentialUnlocalized[7] = "PowerCore";
-		potentialUnlocalized[8] = "AirCanisterEmpty";
-		
-		potentialIcons = new IIcon[potentialUnlocalized.length];
-		cachedIS = new ItemStack[potentialUnlocalized.length];
+		icons = new IIcon[ComponentType.length];
+		itemStackCache = new ItemStack[ComponentType.length];
 	}
 	
-	public ItemStack getItemStack(int damage) {
-		if (damage >=0 && damage < potentialUnlocalized.length) {
-			if (cachedIS[damage] == null) {
-				cachedIS[damage] = new ItemStack(WarpDrive.itemComponent,1,damage);
+	public static ItemStack getItemStack(ComponentType componentType) {
+		if (componentType != null) {
+			int damage = componentType.ordinal();
+			if (itemStackCache[damage] == null) {
+				itemStackCache[damage] = new ItemStack(WarpDrive.itemComponent, 1, damage);
 			}
-			return cachedIS[damage];
+			return itemStackCache[damage];
 		}
 		return null;
 	}
 	
-	public ItemStack getISNoCache(int amount,int damage) {
-		return new ItemStack(WarpDrive.itemComponent, amount, damage);
-	}
-	
-	public void registerRecipes() {
-		GameRegistry.addRecipe(new ShapedOreRecipe(getItemStack(0),false,"nrn","r r","nrn",
-				'r', Items.redstone,
-				'n', Items.gold_nugget));
-		
-		GameRegistry.addRecipe(new ShapedOreRecipe(getItemStack(1),false,"g","e","c",
-				'g', Blocks.glass,
-				'e', Items.ender_pearl,
-				'c', getItemStack(0)));
-			
-		GameRegistry.addRecipe(new ShapedOreRecipe(getItemStack(2),false," g ","ede"," c ",
-				'g', Blocks.glass,
-				'e', Items.ender_pearl,
-				'd', Items.diamond,
-				'c', getItemStack(0)));
-		
-		GameRegistry.addRecipe(new ShapedOreRecipe(getItemStack(3),false," g ","rtr"," c ",
-				'g', Blocks.glass,
-				'r', "dyeBlue",
-				't', Blocks.torch,
-				'c', getItemStack(0)));
-		
-		GameRegistry.addRecipe(new ShapedOreRecipe(getItemStack(4),false," l ","rcr"," l ",
-				'l', "dyeWhite",
-				'r', Items.coal,
-				'c', getItemStack(0)));
-		
-		GameRegistry.addRecipe(new ShapedOreRecipe(getItemStack(5),false,"g  ","gwr","rwr",
-				'g', Items.gold_nugget,
-				'r', Items.redstone,
-				'w', "plankWood"));
-		
-		GameRegistry.addRecipe(new ShapedOreRecipe(getItemStack(6),false,"gig","iri","gig",
-				'g', Items.gold_nugget,
-				'r', Items.redstone,
-				'i', Items.iron_ingot));
-		
-		GameRegistry.addRecipe(new ShapedOreRecipe(getItemStack(7),false,"glg","ldl","glg",
-				'g', Items.gold_nugget,
-				'l', "dyeBlue",
-				'd', Items.diamond));
-		
-		GameRegistry.addRecipe(new ShapedOreRecipe(getItemStack(8),false,"gcg","g g","gcg",
-				'g', Blocks.glass,
-				'c', getItemStack(0)));
-	}
-	
-	public boolean doesMatch(ItemStack is, String unlocalised) {
-		if (is == null) {
-			return false;
-		}
-		if (!(is.getItem() instanceof ItemComponent)) {
-				return false;
-		}
-		String data = potentialUnlocalized[is.getItemDamage()];
-		// WarpDrive.logger.info(data);
-		return data.equals(unlocalised);
+	public static ItemStack getItemStackNoCache(ComponentType componentType, int amount) {
+		return new ItemStack(WarpDrive.itemComponent, amount, componentType.ordinal());
 	}
 	
 	@Override
 	public void registerIcons(IIconRegister par1IconRegister) {
-		for(int i = 0; i < potentialUnlocalized.length; i++) {
-			potentialIcons[i] = par1IconRegister.registerIcon("warpdrive:component" + potentialUnlocalized[i]);
+		for(ComponentType componentType : ComponentType.values()) {
+			icons[componentType.ordinal()] = par1IconRegister.registerIcon("warpdrive:component" + componentType.unlocalizedName);
 		}
 	}
 	
 	@Override
-	public String getUnlocalizedName(ItemStack itemSt) {
-		int damage = itemSt.getItemDamage();
-		if (damage >= 0 && damage < potentialUnlocalized.length) {
-			return "item.warpdrive.crafting." + potentialUnlocalized[damage];
+	public String getUnlocalizedName(ItemStack itemStack) {
+		int damage = itemStack.getItemDamage();
+		if (damage >= 0 && damage < ComponentType.length) {
+			return "item.warpdrive.crafting." + ComponentType.get(damage).unlocalizedName;
 		}
 		return getUnlocalizedName();
 	}
 	
 	@Override
 	public IIcon getIconFromDamage(int damage) {
-		if (damage >= 0 && damage < potentialUnlocalized.length) {
-			return potentialIcons[damage];
+		if (damage >= 0 && damage < ComponentType.length) {
+			return icons[damage];
 		}
-		return potentialIcons[0];
+		return icons[0];
 	}
 	
 	@Override
 	public void getSubItems(Item par1, CreativeTabs par2CreativeTabs, List par3List) {
-		for(int i = 0; i < potentialUnlocalized.length; i++) {
-			par3List.add(new ItemStack(par1, 1, i));
+		for(ComponentType componentType : ComponentType.values()) {
+			par3List.add(new ItemStack(par1, 1, componentType.ordinal()));
 		}
 	}
 	
-	// For empty air cans
+	// For empty air canister
 	@Override
-	public ItemStack fullDrop(ItemStack is) {
-		if (doesMatch(is, "AirCanisterEmpty")) {
-			return WarpDrive.itemAirCanisterFull.fullDrop(is);
-		}
-		return null;
-	}	
-	
-	@Override
-	public ItemStack emptyDrop(ItemStack is) {
-		if (doesMatch(is, "AirCanisterEmpty")) {
-			return WarpDrive.itemAirCanisterFull.emptyDrop(is);
+	public ItemStack fullDrop(ItemStack itemStack) {
+		if (canContainAir(itemStack)) {
+			return WarpDrive.itemAirCanisterFull.fullDrop(itemStack);
 		}
 		return null;
 	}
 	
 	@Override
-	public boolean canContainAir(ItemStack is) {
-		return doesMatch(is, "AirCanisterEmpty");
+	public ItemStack emptyDrop(ItemStack itemStack) {
+		if (canContainAir(itemStack)) {
+			return WarpDrive.itemAirCanisterFull.emptyDrop(itemStack);
+		}
+		return null;
 	}
 	
 	@Override
-	public boolean containsAir(ItemStack is) {
+	public boolean canContainAir(ItemStack itemStack) {
+		return (itemStack.getItem() instanceof ItemComponent && itemStack.getItemDamage() == ComponentType.AIR_CANISTER.ordinal());
+	}
+	
+	@Override
+	public boolean containsAir(ItemStack itemStack) {
 		return false;
 	}
 }
