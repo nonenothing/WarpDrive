66d970ff53b09699494415b0aae3f0d076b4cad6
ClawmanCat <clawmancat@gmail.com>
2016-08-15 21:25:41 +0200

    Cleaned up some code
    Cleaned up some TileEntity code that was supposed to fix the spam-glitch
    bug, but didnt.
    Bug still remains at this time.

--- src/main/java/cr0s/warpdrive/block/detection/TileEntitySiren.java
+++ src/main/java/cr0s/warpdrive/block/detection/TileEntitySiren.java
@@ -5,27 +5,21 @@ import cpw.mods.fml.relauncher.SideOnly;
 import cr0s.warpdrive.SirenSound;
 import cr0s.warpdrive.WarpDrive;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.ISound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 
 public class TileEntitySiren extends TileEntity {
-    private static final int COOLDOWN_LENGTH_TICKS = 5;
-
     public enum SirenState {
-        STOPPED, STARTING, STARTED, STOPPING, COOLDOWN;
+        STARTING, STARTED, STOPPING, STOPPED;
     }
 
     private SirenState state = SirenState.STOPPED;
     private boolean isRaidSiren;
     private String name;
-    private Object sound;
     private float range;
-    private int cooldown = 0;
 
-    /* Because the SirenSound exists only on the client, but the Tile Entity itself
-     *  also exists on the server, we cannot declare a SirenSound object directly, thus
-     *  the sound object is declared as type Object. */
+    @SideOnly(Side.CLIENT)
+    private SirenSound sound;
 
     public TileEntitySiren(String name, boolean isRaidSiren, float range) {
         super();
@@ -45,47 +39,46 @@ public class TileEntitySiren extends TileEntity {
         //Siren sound logic.
         switch (this.state) {
             case STOPPED:
-                if (this.isPowered() && !this.isPlaying()) {
-                    this.startSound();
-                } else if (!this.isPowered() && this.isPlaying()) {
-                    //Better safe than sorry.
-                    this.stopSound();
+                if (this.isPlaying()) {
+                    this.state = SirenState.STOPPING;
+                }
+
+                if (this.isPowered()) {
+                    this.state = SirenState.STARTING;
                 }
 
                 break;
             case STARTING:
-                if (this.isPlaying()) {
+                if (this.startSound()) {
                     this.state = SirenState.STARTED;
-                }
-
-                break;
-            case STARTED:
-                if (this.isPowered()) {
-                    if (!this.isPlaying()) this.startSound();
                 } else {
                     this.state = SirenState.STOPPING;
                 }
 
                 break;
-            case STOPPING:
-                this.stopSound();
-
-                this.cooldown = COOLDOWN_LENGTH_TICKS;
-                this.state = SirenState.COOLDOWN;
+            case STARTED:
+                if (!this.isPowered()) {
+                    this.state = SirenState.STOPPING;
+                } else if (!this.isPlaying()) {
+                    this.state = SirenState.STARTING;
+                }
 
                 break;
-            case COOLDOWN:
-                if (cooldown > 0) {
-                    cooldown--;
+            case STOPPING:
+                if (this.isPlaying()) {
+                    this.stopSound();
                 } else {
-                    this.cooldown = 0;
                     this.state = SirenState.STOPPED;
                 }
 
                 break;
             default:
-                this.stopSound();
-                this.state = SirenState.STOPPED;
+                if (this.isPlaying()) {
+                    this.state = SirenState.STOPPING;
+                } else {
+                    this.state = SirenState.STOPPED;
+                }
+
                 break;
         }
     }
@@ -97,8 +90,7 @@ public class TileEntitySiren extends TileEntity {
         super.onChunkUnload();
     }
 
-    //Stops the siren when the TileEntity object is destroyed.
-    //The siren should already be stopped by this point, but better safe than sorry.
+    //Stops the siren when the TileEntity object is invalidated.
     @Override
     public void invalidate() {
         if (worldObj.isRemote && this.isPlaying()) this.stopSound();
@@ -108,36 +100,33 @@ public class TileEntitySiren extends TileEntity {
     //Create a new SirenSound object that the siren will use.
     @SideOnly(Side.CLIENT)
     private void setSound() {
+        System.out.println("Reset sound for TileEntity at " + this.xCoord + ", " + this.yCoord + ", " + this.zCoord + ".");
+
         String resource = WarpDrive.MODID + ":siren_" + (isRaidSiren ? "raid" : "industrial");
         this.sound = new SirenSound(new ResourceLocation(resource), this.range, this.xCoord, this.yCoord, this.zCoord);
     }
 
     //Forces the siren to start playing its sound;
     @SideOnly(Side.CLIENT)
-    public void startSound() {
+    public boolean startSound() {
         try {
-            Minecraft.getMinecraft().getSoundHandler().playSound((ISound) sound);
-            this.state = SirenState.STARTING;
+            Minecraft.getMinecraft().getSoundHandler().playSound(sound);
+            return true;
         } catch (IllegalArgumentException e) {
-			/* If soundHandler.playSound() is called before the previous same ISound stops
-			 * from a call to soundHandler.stopSound(), an IllegalArgumentException will
-			 * be thrown. This happens when you spam the redstone trigger.
-			 * We're trying to recover by forcing a stop. */
-
-            this.state = SirenState.STOPPING;
+            return false;
         }
     }
 
     //Forces the siren to stop playing its sound.
     @SideOnly(Side.CLIENT)
     public void stopSound() {
-        Minecraft.getMinecraft().getSoundHandler().stopSound((ISound) sound);
+        Minecraft.getMinecraft().getSoundHandler().stopSound(sound);
     }
 
     //Checks if the siren is currently playing its sound.
     @SideOnly(Side.CLIENT)
     public boolean isPlaying() {
-        return Minecraft.getMinecraft().getSoundHandler().isSoundPlaying((ISound) sound);
+        return Minecraft.getMinecraft().getSoundHandler().isSoundPlaying(sound);
     }
 
     //Checks if the siren is being powered by redstone.
