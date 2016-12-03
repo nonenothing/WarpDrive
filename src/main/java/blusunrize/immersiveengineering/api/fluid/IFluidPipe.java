ab23f7673cf33a7a871c15955cd05b5f33498ea0
LemADEC <lemadec.fr@gmail.com>
2016-12-03 13:32:48 +0100

    Updated ImmersiveEngineering to 0.10+ with Maven
    Updated OpenComputers to 1.6.0+ with Maven

--- src/main/java/blusunrize/immersiveengineering/api/fluid/IFluidPipe.java
+++
@@ -1,9 +0,0 @@
-package blusunrize.immersiveengineering.api.fluid;
-
-import net.minecraft.util.EnumFacing;
-
-public interface IFluidPipe
-{
-	boolean canOutputPressurized(boolean consumePower);
-	boolean hasOutputConnection(EnumFacing side);
-}
--- src/main/java/blusunrize/immersiveengineering/api/shader/IShaderItem.java
+++
@@ -1,8 +0,0 @@
-package blusunrize.immersiveengineering.api.shader;
-
-import net.minecraft.item.ItemStack;
-
-public interface IShaderItem
-{
-	ShaderCase getShaderCase(ItemStack shader, ItemStack item, String shaderType);
-}
