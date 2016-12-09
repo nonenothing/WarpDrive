1f39c41f3d7d93674a16016973efb428edcd0f0b
TxN <AimII@yandex.ru>
2016-12-10 04:59:35 +0700

    Added computer control to air generator for OC & CC (#226)

--- src/main/java/cr0s/warpdrive/block/TileEntityAirGenerator.java
+++ src/main/java/cr0s/warpdrive/block/TileEntityAirGenerator.java
@@ -2,12 +2,20 @@ package cr0s.warpdrive.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.MathHelper;
 import net.minecraftforge.common.util.ForgeDirection;
+import cpw.mods.fml.common.Optional;
 import cr0s.warpdrive.WarpDrive;
 import cr0s.warpdrive.config.WarpDriveConfig;
+import dan200.computercraft.api.lua.ILuaContext;
+import dan200.computercraft.api.peripheral.IComputerAccess;
+import li.cil.oc.api.machine.Arguments;
+import li.cil.oc.api.machine.Callback;
+import li.cil.oc.api.machine.Context;
 
 public class TileEntityAirGenerator extends TileEntityAbstractEnergy {
 	private int cooldownTicks = 0;
+	private boolean isEnabled = true;
 	private static final int START_CONCENTRATION_VALUE = 15;
 	
 	public TileEntityAirGenerator() {
@@ -35,10 +43,10 @@ public class TileEntityAirGenerator extends TileEntityAbstractEnergy {
 			}
 			return;
 		}
-		
+
 		cooldownTicks++;
 		if (cooldownTicks > WarpDriveConfig.AIRGEN_AIR_GENERATION_TICKS) {
-			if (energy_consume(WarpDriveConfig.AIRGEN_ENERGY_PER_NEWAIRBLOCK, true)) {
+			if (isEnabled && energy_consume(WarpDriveConfig.AIRGEN_ENERGY_PER_NEWAIRBLOCK, true)) {
 				if (getBlockMetadata() != 1) {
 					worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 1, 2); // set enabled texture
 				}
@@ -62,7 +70,7 @@ public class TileEntityAirGenerator extends TileEntityAbstractEnergy {
 		Block block = worldObj.getBlock(xCoord + xOffset, yCoord + yOffset, zCoord + zOffset);
 		if (block.isAir(worldObj, xCoord + xOffset, yCoord + yOffset, zCoord + zOffset)) {// can be air
 			int energy_cost = (!block.isAssociatedBlock(WarpDrive.blockAir)) ? WarpDriveConfig.AIRGEN_ENERGY_PER_NEWAIRBLOCK : WarpDriveConfig.AIRGEN_ENERGY_PER_EXISTINGAIRBLOCK;
-			if (energy_consume(energy_cost, true)) {// enough energy
+			if (isEnabled && energy_consume(energy_cost, true)) {// enough energy and enabled
 				if (worldObj.setBlock(xCoord + xOffset, yCoord + yOffset, zCoord + zOffset, WarpDrive.blockAir, START_CONCENTRATION_VALUE, 2)) {
 					// (needs to renew air or was not maxed out)
 					energy_consume(WarpDriveConfig.AIRGEN_ENERGY_PER_NEWAIRBLOCK, false);
@@ -87,11 +95,13 @@ public class TileEntityAirGenerator extends TileEntityAbstractEnergy {
 	@Override
 	public void readFromNBT(NBTTagCompound tag) {
 		super.readFromNBT(tag);
+		isEnabled = tag.getBoolean("isEnabled");
 	}
 	
 	@Override
 	public void writeToNBT(NBTTagCompound tag) {
 		super.writeToNBT(tag);
+		tag.setBoolean("isEnabled", isEnabled);
 	}
 	
 	@Override
@@ -110,5 +120,33 @@ public class TileEntityAirGenerator extends TileEntityAbstractEnergy {
 		getClass().getSimpleName(),
 		worldObj == null ? "~NULL~" : worldObj.getWorldInfo().getWorldName(),
 		xCoord, yCoord, zCoord);
+	}
+	
+	public Object[] enable(Object[] arguments) {
+		if (arguments.length == 1) {
+			isEnabled = toBool(arguments[0]);
+		}
+		return new Object[] { isEnabled };
+	}
+	
+	// OpenComputer callback methods
+	@Callback
+	@Optional.Method(modid = "OpenComputers")
+	public Object[] enable(Context context, Arguments arguments) {
+			return enable(argumentsOCtoCC(arguments));
+	}
+	
+	// ComputerCraft IPeripheral methods implementation
+	@Override
+	@Optional.Method(modid = "ComputerCraft")
+	public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) {
+		String methodName = getMethodName(method);
+		
+		switch (methodName) {
+			case "enable": 
+				return enable(arguments);		
+		}
+		
+		return super.callMethod(computer, context, method, arguments);
 	}
 }
