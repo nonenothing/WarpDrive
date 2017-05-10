3d4bea2f29ed60a08a425795061043c97f81a458
LemADEC <lemadec.fr@gmail.com>
2017-05-10 04:04:03 +0200

    Fixed inconsistent max values for Video and Control channels Code cleanup

--- src/main/java/cr0s/warpdrive/api/IControlChannel.java
+++ src/main/java/cr0s/warpdrive/api/IControlChannel.java
@@ -2,7 +2,7 @@ package cr0s.warpdrive.api;
 
 public interface IControlChannel {
 	int CONTROL_CHANNEL_MIN = 0;
-	int CONTROL_CHANNEL_MAX = 999999999;
+	int CONTROL_CHANNEL_MAX = 0xFFFFFFF;    // 268435455
 	String CONTROL_CHANNEL_TAG = "controlChannel";
 	
 	// get control channel, return -1 if invalid 
