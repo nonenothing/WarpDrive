c895d9aeb0ae3f82aa978e0f6320f275d136f6ca
LemADEC <lemadec.fr@gmail.com>
2017-11-01 15:49:33 +0100

    Fixed NPE crash when an LHC has no injector

--- src/main/java/cr0s/warpdrive/data/AcceleratorSetup.java
+++ src/main/java/cr0s/warpdrive/data/AcceleratorSetup.java
@@ -547,7 +547,7 @@ public class AcceleratorSetup extends GlobalPosition {
 	
 	// Pseudo-API for computers
 	public Object[][] getControlPoints(final IBlockAccess blockAccess) {
-		final Object[][] objectResults  = new Object[controlPoints.size() + keyInjectors.length][];
+		final Object[][] objectResults  = new Object[controlPoints.size() + (keyInjectors == null ? 0 : keyInjectors.length)][];
 		int index = 0;
 		for (final Entry<VectorI, Integer> entryControlPoint : controlPoints.entrySet()) {
 			final Integer tier = TrajectoryPoint.getTier(entryControlPoint.getValue());
