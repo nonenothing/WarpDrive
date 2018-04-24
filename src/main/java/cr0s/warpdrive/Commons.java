nonenothing/WarpDrive

commit 50c327521068aa8c889bab9c1bb4049b4b40be7e

Added filter for redundant tooltips

src/main/java/cr0s/warpdrive/Commons.java
@@ -111,8 +111,30 @@ public class Commons {
 	// add tooltip information with text formatting and line splitting
 	// will ensure it fits on minimum screen width
 	public static void addTooltip(final List<String> list, final String tooltip) {
+		// skip empty tooltip
+		if (tooltip.isEmpty()) {
+			return;
+		}
+		
+		// apply requested formatting
 		final String[] split = updateEscapeCodes(tooltip).split("\n");
+		
+		// add new lines
 		for (final String line : split) {
+			// skip redundant information
+			boolean isExisting = false;
+			for (final String lineExisting : list) {
+				if ( lineExisting.contains(split[0])
+				     || split[0].contains(lineExisting) ) {
+					isExisting = true;
+					break;
+				}
+			}
+			if (isExisting) {
+				continue;
+			}
+			
+			// apply screen formatting/cesure
 			String lineRemaining = line;
 			String formatNextLine = "";
 			while (!lineRemaining.isEmpty()) {

commit 5f8e465c2a2b2ced5a68e714c7a2eb4a29e8052a

Fixed missing tooltip lines due to over-deduplication
This was a regression introduced in 50c327521068aa8c889bab9c1bb4049b4b40be7e, no impact on public versions of the mod

src/main/java/cr0s/warpdrive/Commons.java
@@ -132,8 +132,8 @@ public class Commons {
 			// skip redundant information
 			boolean isExisting = false;
 			for (final String lineExisting : list) {
-				if ( lineExisting.contains(split[0])
-				  || split[0].contains(lineExisting) ) {
+				if ( lineExisting.contains(line)
+				  || line.contains(lineExisting) ) {
 					isExisting = true;
 					break;
 				}