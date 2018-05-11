nonenothing/WarpDrive

commit 98c51ea3d4fb439a6bb2965efef36612a048ce83

Fixed StarMapRegistry not updating renamed entries
Updated API for StarMapRegistryItem

src/main/java/cr0s/warpdrive/data/StarMapRegistry.java
@@ -5,7 +5,6 @@ import cr0s.warpdrive.WarpDrive;
 import cr0s.warpdrive.api.IStarMapRegistryTileEntity;
 import cr0s.warpdrive.block.movement.TileEntityShipCore;
 import cr0s.warpdrive.config.WarpDriveConfig;
-import cr0s.warpdrive.data.StarMapRegistryItem.EnumStarMapEntryType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -58,13 +57,17 @@ public class StarMapRegistry {
 		if (setRegistryItems == null) {
 			setRegistryItems = new CopyOnWriteArraySet<>();
 		}
+		final ArrayList<StarMapRegistryItem> listToRemove = new ArrayList<>(3);
 		for (final StarMapRegistryItem registryItem : setRegistryItems) {
-			if (registryItem.sameIdOrCoordinates(tileEntity)) {
-				// already registered
+			if ( registryItem.type.getName().equals(tileEntity.getStarMapType())
+			  && registryItem.uuid.equals(tileEntity.getUUID()) ) {// already registered
 				registryItem.update(tileEntity);    // @TODO probably not thread safe
 				return;
+			} else if (registryItem.sameCoordinates(tileEntity)) {
+				listToRemove.add(registryItem);
 			}
 		}
+		setRegistryItems.removeAll(listToRemove);
 		
 		// not found => add
 		countAdd++;
@@ -86,7 +89,7 @@ public class StarMapRegistry {
 		}
 		
 		for (final StarMapRegistryItem registryItem : setRegistryItems) {
-			if (registryItem.isSameTileEntity(tileEntity)) {
+			if (registryItem.sameCoordinates(tileEntity)) {
 				// found it, remove and exit
 				countRemove++;
 				setRegistryItems.remove(registryItem);

src/main/java/cr0s/warpdrive/api/IStarMapRegistryTileEntity.java
@@ -1,5 +1,6 @@
 package cr0s.warpdrive.api;
 
+import cr0s.warpdrive.data.EnumStarMapEntryType;
 import cr0s.warpdrive.data.VectorI;
 
 import java.util.UUID;
@@ -10,7 +11,7 @@ import net.minecraft.util.AxisAlignedBB;
 public interface IStarMapRegistryTileEntity {
 	
 	// get the registry type
-	String getStarMapType();
+	EnumStarMapEntryType getStarMapType();
 	
 	// get the unique id
 	UUID getUUID();

src/main/java/cr0s/warpdrive/block/atomic/TileEntityAcceleratorController.java
@@ -2,7 +2,7 @@ package cr0s.warpdrive.block.atomic;
 
 import cr0s.warpdrive.api.IStarMapRegistryTileEntity;
 import cr0s.warpdrive.block.TileEntityAbstractEnergy;
-import cr0s.warpdrive.data.StarMapRegistryItem.EnumStarMapEntryType;
+import cr0s.warpdrive.data.EnumStarMapEntryType;
 import cr0s.warpdrive.data.VectorI;
 
 import java.util.UUID;
@@ -23,8 +23,8 @@ public class TileEntityAcceleratorController extends TileEntityAbstractEnergy im
 	
 	// IStarMapRegistryTileEntity overrides
 	@Override
-	public String getStarMapType() {
-		return EnumStarMapEntryType.ACCELERATOR.getName();
+	public EnumStarMapEntryType getStarMapType() {
+		return EnumStarMapEntryType.ACCELERATOR;
 	}
 	
 	@Override

src/main/java/cr0s/warpdrive/block/movement/TileEntityShipCore.java
@@ -12,7 +12,7 @@ import cr0s.warpdrive.data.EnumShipControllerCommand;
 import cr0s.warpdrive.data.EnumShipCoreState;
 import cr0s.warpdrive.data.EnumShipMovementType;
 import cr0s.warpdrive.data.Jumpgate;
-import cr0s.warpdrive.data.StarMapRegistryItem.EnumStarMapEntryType;
+import cr0s.warpdrive.data.EnumStarMapEntryType;
 import cr0s.warpdrive.data.Vector3;
 import cr0s.warpdrive.data.VectorI;
 import cr0s.warpdrive.event.JumpSequencer;
@@ -1198,8 +1198,8 @@ public class TileEntityShipCore extends TileEntityAbstractEnergy implements ISta
 	
 	// IStarMapRegistryTileEntity overrides
 	@Override
-	public String getStarMapType() {
-		return EnumStarMapEntryType.SHIP.getName();
+	public EnumStarMapEntryType getStarMapType() {
+		return EnumStarMapEntryType.SHIP;
 	}
 	
 	@Override

src/main/java/cr0s/warpdrive/block/movement/TileEntityTransporterBeacon.java
@@ -8,7 +8,7 @@ import cr0s.warpdrive.block.TileEntityAbstractEnergy;
 import cr0s.warpdrive.config.WarpDriveConfig;
 import cr0s.warpdrive.data.EnumTransporterBeaconState;
 import cr0s.warpdrive.data.StarMapRegistryItem;
-import cr0s.warpdrive.data.StarMapRegistryItem.EnumStarMapEntryType;
+import cr0s.warpdrive.data.EnumStarMapEntryType;
 import dan200.computercraft.api.lua.ILuaContext;
 import dan200.computercraft.api.peripheral.IComputerAccess;
 import li.cil.oc.api.machine.Arguments;

src/main/java/cr0s/warpdrive/block/movement/TileEntityTransporterCore.java
@@ -21,7 +21,7 @@ import cr0s.warpdrive.data.ForceFieldSetup;
 import cr0s.warpdrive.data.GlobalPosition;
 import cr0s.warpdrive.data.MovingEntity;
 import cr0s.warpdrive.data.StarMapRegistry;
-import cr0s.warpdrive.data.StarMapRegistryItem.EnumStarMapEntryType;
+import cr0s.warpdrive.data.EnumStarMapEntryType;
 import cr0s.warpdrive.data.Vector3;
 import cr0s.warpdrive.data.VectorI;
 import cr0s.warpdrive.network.PacketHandler;
@@ -479,8 +479,8 @@ public class TileEntityTransporterCore extends TileEntityAbstractEnergy implemen
 	
 	// IStarMapRegistryTileEntity overrides
 	@Override
-	public String getStarMapType() {
-		return EnumStarMapEntryType.TRANSPORTER.getName();
+	public EnumStarMapEntryType getStarMapType() {
+		return EnumStarMapEntryType.TRANSPORTER;
 	}
 	
 	@Override

src/main/java/cr0s/warpdrive/data/EnumStarMapEntryType.java
@@ -1,0 +1,46 @@
+package cr0s.warpdrive.data;
+
+import java.util.HashMap;
+
+public enum EnumStarMapEntryType {
+	UNDEFINED  (0, "-undefined-"),
+	SHIP       (1, "ship"       ), // a ship core
+	JUMPGATE   (2, "jumpgate"   ), // a jump gate
+	PLANET     (3, "planet"     ), // a planet (a transition plane allowing to move to another dimension)
+	STAR       (4, "star"       ), // a star
+	STRUCTURE  (5, "structure"  ), // a structure from WorldGeneration (moon, asteroid field, etc.)
+	WARP_ECHO  (6, "warp_echo"  ), // remains of a warp
+	ACCELERATOR(7, "accelerator"), // an accelerator setup
+	TRANSPORTER(8, "transporter"); // a transporter room
+	
+	private final int id;
+	private final String name;
+	
+	// cached values
+	public static final int length;
+	private static final HashMap<String, EnumStarMapEntryType> mapNames = new HashMap<>();
+	
+	static {
+		length = EnumStarMapEntryType.values().length;
+		for (final EnumStarMapEntryType enumStarMapEntryType : values()) {
+			mapNames.put(enumStarMapEntryType.getName(), enumStarMapEntryType);
+		}
+	}
+	
+	EnumStarMapEntryType(final int id, final String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public static EnumStarMapEntryType getByName(final String name) {
+		return mapNames.get(name);
+	}
+}

src/main/java/cr0s/warpdrive/data/StarMapRegistryItem.java
@@ -2,9 +2,7 @@ package cr0s.warpdrive.data;
 
 import cr0s.warpdrive.WarpDrive;
 import cr0s.warpdrive.api.IStarMapRegistryTileEntity;
-import cr0s.warpdrive.config.WarpDriveConfig;
 
-import java.util.HashMap;
 import java.util.UUID;
 
 import net.minecraft.nbt.NBTTagCompound;
@@ -20,49 +18,6 @@ public class StarMapRegistryItem extends GlobalPosition {
 	public int mass;
 	public double isolationRate = 0.0D;
 	public String name = "default";
-	
-	public enum EnumStarMapEntryType {
-		UNDEFINED(0, "-undefined-"),
-		SHIP(1, "ship"),	        	// a ship core
-		JUMPGATE(2, "jumpgate"),	    // a jump gate
-		PLANET(3, "planet"),		    // a planet (a transition plane allowing to move to another dimension)
-		STAR(4, "star"),    		    // a star
-		STRUCTURE(5, "structure"),	    // a structure from WorldGeneration (moon, asteroid field, etc.)
-		WARP_ECHO(6, "warp_echo"),	    // remains of a warp
-		ACCELERATOR(7, "accelerator"),	// an accelerator setup
-		TRANSPORTER(8, "transporter");	// a transporter room
-		
-		private final int id;
-		private final String name;
-		
-		// cached values
-		public static final int length;
-		private static final HashMap<String, EnumStarMapEntryType> mapNames = new HashMap<>();
-		
-		static {
-			length = EnumStarMapEntryType.values().length;
-			for (final EnumStarMapEntryType enumStarMapEntryType : values()) {
-				mapNames.put(enumStarMapEntryType.getName(), enumStarMapEntryType);
-			}
-		}
-		
-		EnumStarMapEntryType(final int id, final String name) {
-			this.id = id;
-			this.name = name;
-		}
-		
-		public int getId() {
-			return id;
-		}
-		
-		public String getName() {
-			return name;
-		}
-		
-		public static EnumStarMapEntryType getByName(final String name) {
-			return mapNames.get(name);
-		}
-	}
 	
 	public StarMapRegistryItem(
 	                          final EnumStarMapEntryType type, final UUID uuid,
@@ -95,7 +50,7 @@ public class StarMapRegistryItem extends GlobalPosition {
 	
 	public StarMapRegistryItem(final IStarMapRegistryTileEntity tileEntity) {
 		this(
-			EnumStarMapEntryType.getByName(tileEntity.getStarMapType()), tileEntity.getUUID(),
+			tileEntity.getStarMapType(), tileEntity.getUUID(),
 			((TileEntity) tileEntity).getWorldObj().provider.dimensionId,
 			((TileEntity) tileEntity).xCoord, ((TileEntity) tileEntity).yCoord, ((TileEntity) tileEntity).zCoord,
 			tileEntity.getStarMapArea(),
@@ -103,10 +58,9 @@ public class StarMapRegistryItem extends GlobalPosition {
 			tileEntity.getStarMapName());
 	}
 	
-	public boolean sameIdOrCoordinates(final IStarMapRegistryTileEntity tileEntity) {
+	public boolean sameCoordinates(final IStarMapRegistryTileEntity tileEntity) {
 		assert(tileEntity instanceof TileEntity);
-		return uuid == tileEntity.getUUID()
-			|| dimensionId == ((TileEntity) tileEntity).getWorldObj().provider.dimensionId
+		return dimensionId == ((TileEntity) tileEntity).getWorldObj().provider.dimensionId
 			&& x == ((TileEntity) tileEntity).xCoord
 			&& y == ((TileEntity) tileEntity).yCoord
 			&& z == ((TileEntity) tileEntity).zCoord;
@@ -114,8 +68,9 @@ public class StarMapRegistryItem extends GlobalPosition {
 	
 	public void update(final IStarMapRegistryTileEntity tileEntity) {
 		if (WarpDrive.isDev) {
-			assert (tileEntity instanceof TileEntity);
-			assert (sameIdOrCoordinates(tileEntity));
+			assert(tileEntity instanceof TileEntity);
+			assert(type == tileEntity.getStarMapType());
+			assert(uuid.equals(tileEntity.getUUID()));
 		}
 		final AxisAlignedBB aabbArea = tileEntity.getStarMapArea();
 		if (aabbArea != null) {
@@ -129,14 +84,6 @@ public class StarMapRegistryItem extends GlobalPosition {
 		mass = tileEntity.getMass();
 		isolationRate = tileEntity.getIsolationRate();
 		name = tileEntity.getStarMapName();
-	}
-	
-	public boolean isSameTileEntity(final IStarMapRegistryTileEntity tileEntity) {
-		assert(tileEntity instanceof TileEntity);
-		return dimensionId == ((TileEntity) tileEntity).getWorldObj().provider.dimensionId
-		  && x == ((TileEntity) tileEntity).xCoord
-		  && y == ((TileEntity) tileEntity).yCoord
-		  && z == ((TileEntity) tileEntity).zCoord;
 	}
 	
 	public boolean contains(final int x, final int y, final int z) {
@@ -205,10 +152,12 @@ public class StarMapRegistryItem extends GlobalPosition {
 	
 	@Override
 	public String toString() {
-		return String.format("%s @ DIM%d (%d %d %d) (%d %d %d) -> (%d %d %d)",
-			getClass().getSimpleName(), dimensionId,
-			x, y, z,
-			minX, minY, minZ,
-			maxX, maxY, maxZ);
+		return String.format("%s '%s' %s @ DIM%d (%d %d %d) (%d %d %d) -> (%d %d %d)",
+		                     getClass().getSimpleName(),
+			                 type, uuid,
+			                 dimensionId,
+			                 x, y, z,
+			                 minX, minY, minZ,
+			                 maxX, maxY, maxZ);
 	}
 }

commit 1482580995c2ee6cfd539be963ac63b7cceec3d8

Fixed a regression in StarMapRegistry introduced in 98c51ea3d4fb439a6bb2965efef36612a048ce83

src/main/java/cr0s/warpdrive/data/StarMapRegistry.java
@@ -1,5 +1,6 @@
 package cr0s.warpdrive.data;
 
+import cr0s.warpdrive.Commons;
 import cr0s.warpdrive.LocalProfiler;
 import cr0s.warpdrive.WarpDrive;
 import cr0s.warpdrive.api.IStarMapRegistryTileEntity;
@@ -45,23 +46,41 @@ public class StarMapRegistry {
 	}
 	
 	public void updateInRegistry(final IStarMapRegistryTileEntity tileEntity) {
+		// validate context
 		assert(tileEntity instanceof TileEntity);
+		if (!Commons.isSafeThread()) {
+			WarpDrive.logger.error(String.format("Non-threadsafe call to StarMapRegistry:updateInRegistry outside main thread, for %s",
+			                                     tileEntity));
+		}
 		
+		// update statistics
 		countRead++;
 		if (WarpDriveConfig.LOGGING_STARMAP) {
 			if (countRead % 1000 == 0) {
 				WarpDrive.logger.info("Starmap registry stats: read " + countRead + " add " + countAdd + " remove " + countRemove + " => " + ((float) countRead) / (countRemove + countRead + countAdd) + "% read");
 			}
 		}
+		
+		// get dimension
 		CopyOnWriteArraySet<StarMapRegistryItem> setRegistryItems = registry.get(((TileEntity) tileEntity).getWorldObj().provider.dimensionId);
 		if (setRegistryItems == null) {
 			setRegistryItems = new CopyOnWriteArraySet<>();
 		}
+		
+		// get entry
 		final ArrayList<StarMapRegistryItem> listToRemove = new ArrayList<>(3);
+		final UUID uuidTileEntity = tileEntity.getUUID();
 		for (final StarMapRegistryItem registryItem : setRegistryItems) {
-			if ( registryItem.type.getName().equals(tileEntity.getStarMapType())
-			  && registryItem.uuid.equals(tileEntity.getUUID()) ) {// already registered
-				registryItem.update(tileEntity);    // @TODO probably not thread safe
+			if (registryItem.uuid == null) {
+				WarpDrive.logger.error(String.format("Removing invalid StarMapRegistryItem %s",
+				                                     registryItem));
+				listToRemove.add(registryItem);
+				continue;
+			}
+			
+			if ( registryItem.type.equals(tileEntity.getStarMapType())
+			  && registryItem.uuid.equals(uuidTileEntity) ) {// already registered
+				registryItem.update(tileEntity);    // in-place update only works as long as hashcode remains unchanged
 				return;
 			} else if (registryItem.sameCoordinates(tileEntity)) {
 				listToRemove.add(registryItem);