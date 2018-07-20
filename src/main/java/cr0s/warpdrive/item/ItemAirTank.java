6181c7762d761bb78339b7d1a762ef58ad133fe2
Unknown <lemadec.fr@gmail.com>
2018-07-20 10:11:51 +0200

    Updated air tanks rendering to 1.12

--- src/main/java/cr0s/warpdrive/item/ItemAirTank.java
+++ src/main/java/cr0s/warpdrive/item/ItemAirTank.java
@@ -24,34 +24,6 @@ public class ItemAirTank extends ItemAbstractBase implements IAirContainerItem {
 		setUnlocalizedName("warpdrive.breathing.air_tank." + enumAirTankTier.getName());
 	}
 	
-	//	icons[ 0] = iconRegister.registerIcon("warpdrive:breathing/air_canister");
-	//	icons[ 1] = iconRegister.registerIcon("warpdrive:breathing/air_canister");
-	//	icons[ 2] = iconRegister.registerIcon("warpdrive:breathing/air_canister");
-	//	icons[ 3] = iconRegister.registerIcon("warpdrive:breathing/air_canister");
-	//	icons[ 4] = iconRegister.registerIcon("warpdrive:breathing/air_canister");
-	//	icons[ 5] = iconRegister.registerIcon("warpdrive:breathing/air_canister");
-	//	icons[ 6] = iconRegister.registerIcon("warpdrive:breathing/air_tank1-0");
-	//	icons[ 7] = iconRegister.registerIcon("warpdrive:breathing/air_tank1-20");
-	//	icons[ 8] = iconRegister.registerIcon("warpdrive:breathing/air_tank1-40");
-	//	icons[ 9] = iconRegister.registerIcon("warpdrive:breathing/air_tank1-60");
-	//	icons[10] = iconRegister.registerIcon("warpdrive:breathing/air_tank1-80");
-	//	icons[11] = iconRegister.registerIcon("warpdrive:breathing/air_tank1-100");
-	//	icons[12] = iconRegister.registerIcon("warpdrive:breathing/air_tank2-0");
-	//	icons[13] = iconRegister.registerIcon("warpdrive:breathing/air_tank2-20");
-	//	icons[14] = iconRegister.registerIcon("warpdrive:breathing/air_tank2-40");
-	//	icons[15] = iconRegister.registerIcon("warpdrive:breathing/air_tank2-60");
-	//	icons[16] = iconRegister.registerIcon("warpdrive:breathing/air_tank2-80");
-	//	icons[17] = iconRegister.registerIcon("warpdrive:breathing/air_tank2-100");
-	//	icons[18] = iconRegister.registerIcon("warpdrive:breathing/air_tank3-0");
-	//	icons[19] = iconRegister.registerIcon("warpdrive:breathing/air_tank3-20");
-	//	icons[20] = iconRegister.registerIcon("warpdrive:breathing/air_tank3-40");
-	//	icons[21] = iconRegister.registerIcon("warpdrive:breathing/air_tank3-60");
-	//	icons[22] = iconRegister.registerIcon("warpdrive:breathing/air_tank3-80");
-	//	icons[23] = iconRegister.registerIcon("warpdrive:breathing/air_tank3-100");
-	//	final double ratio = 1.0D - damage / (double) getMaxDamage();
-	//	final int offset = (ratio <= 0.0) ? 0 : (ratio < 0.2) ? 1 : (ratio < 0.4) ? 2 : (ratio < 0.6) ? 3 : (ratio < 0.8) ? 4 : 5;
-	//	return icons[Math.min(icons.length, offset + tier * 6)];
-	
 	@Override
 	public void getSubItems(@Nonnull final CreativeTabs creativeTab, @Nonnull final NonNullList<ItemStack> list) {
 		if (!isInCreativeTab(creativeTab)) {
